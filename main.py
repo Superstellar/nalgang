27351fdd9eb20d74da2d7b15ea66a4d99e6f2d76
Superstellar <38059780+Superstellar@users.noreply.github.com>
2020-04-16 23:13:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ if not __name__=="__main__":
 os.chdir(sys.path[0]) # change working dir as script dir to get fixed relative access to data
 if not os.path.exists("data"): os.makedirs("data")
 
-TOKEN = 'NjkyNzM2NzgxNTUyMTg5NTgx.XphjSg.1VxHBhvNhkqKZ8WdHEa1LtUU-28' #$os.environ["nalgang_TOKEN"]
+TOKEN = os.environ["nalgang_TOKEN"]
 client.run(TOKEN)
